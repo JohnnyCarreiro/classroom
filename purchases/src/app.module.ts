d7446f71bd8a970d268c1011e9edfffa28330db8
Johnny Carreiro <johnny@johnnycarreiro.com>
2023-07-04 23:45:40 -0300

    feat(module): ddeclare auth module on main app instance

--- purchases/src/app.module.ts
+++ purchases/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from './database/database.module'
 import { HttpModule } from './http/http.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [DatabaseModule, HttpModule, AuthModule],
   controllers: [],
   providers: []
 })
